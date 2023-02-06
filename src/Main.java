9b8a38c025344eab1e8c2abd0740f9db49a79752
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2023-02-06 19:32:11 +0700

    Sodienthoai

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sodienthoai.fxml"));
+        primaryStage.setScene(new Scene(root,800,600));
+        primaryStage.show();
+    }
+}
