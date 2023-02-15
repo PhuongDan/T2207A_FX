d4c163124bb10a50cbd9692e2fee0a679b22007c
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2023-02-15 16:11:14 +0700

    Sodienthoai

--- src/controllers/ListClassesController.java
+++ src/controllers/ListClassesController.java
@@ -1,20 +1,30 @@
 package controllers;
 
+import database.Database;
 import entities.Classes;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
-public class ListClassesController {
-    public static Classes editClasses;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class ListClassesController implements Initializable {
     public TableView<Classes> tbviewcl;
+    public TableColumn<Classes,Integer> cID;
     public TableColumn<Classes,String> cName;
     public TableColumn<Classes,String> cRoom;
-    public TableColumn<Classes, Button> cAction;
+//    public TableColumn<Classes, Button> cAction;
 
 
     public void goToHome(ActionEvent actionEvent)throws Exception {
@@ -25,4 +35,39 @@ public class ListClassesController {
     public void goToCreate(ActionEvent actionEvent)throws Exception { Parent root = FXMLLoader.load(getClass().getResource("../resources/classes/create.fxml"));
         HomeController.rootStage.setScene(new Scene(root,600,400));
     }
-}
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        cID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        cName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        cRoom.setCellValueFactory(new PropertyValueFactory<>("room"));
+        ObservableList<Classes> list = FXCollections.observableArrayList();
+        try {
+            Database db = new Database();
+            Statement stt = db.getStatement();
+            String sql = "select * from lophoc";
+            ResultSet rs = stt.executeQuery(sql);
+            while (rs.next()){
+               Integer id = rs.getInt("id");
+               String name = rs.getString("name");
+               String room = rs.getString("room");
+               Classes c = new Classes(id,name,room);
+               list.add(c);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        tbviewcl.setItems(list);
+    }
+
+
+//        @Override
+//        public void initialize(URL location, ResourceBundle resources) {
+//            cName.setCellValueFactory(new PropertyValueFactory<>("name"));
+//            cRoom.setCellValueFactory(new PropertyValueFactory<>("room"));
+//            cAction.setCellValueFactory(new PropertyValueFactory<>("edit"));
+//
+//        }
+    }
+
--- src/controllers/ListStudentsController.java
+++ src/controllers/ListStudentsController.java
@@ -1,12 +1,35 @@
 package controllers;
 
+import database.Database;
+import entities.Student;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class ListStudentsController {
+import java.net.URL;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class ListStudentsController implements Initializable {
+
+    public TableView<Student> tbstu;
+    public TableColumn<Student,Integer> cID;
+    public TableColumn<Student,String> cName;
+    public TableColumn<Student,String> cEmail;
+    public TableColumn<Student,Date> cBirthday;
+    public TableColumn<Student,String> cGender;
+    public TableColumn<Student,Integer> cClass_id;
     public void goToHome(ActionEvent actionEvent) throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource("../resources/home.fxml"));
         HomeController.rootStage.setScene(new Scene(root,600,400));
     }
@@ -14,5 +37,35 @@ public class ListStudentsController {
     public void goToCreate(ActionEvent actionEvent)throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../resources/students/create.fxml"));
         HomeController.rootStage.setScene(new Scene(root,600,400));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        cName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        cEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        cBirthday.setCellValueFactory(new PropertyValueFactory<>("birthday"));
+        cGender.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        cClass_id.setCellValueFactory(new PropertyValueFactory<>("class_id"));
+        ObservableList<Student> list = FXCollections.observableArrayList();
+        try {
+            Database db = new Database();
+            Statement stt = db.getStatement();
+            String sql = "select * from sinhvien";
+            ResultSet rs = stt.executeQuery(sql);
+            while (rs.next()){
+                Integer id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date birthday = rs.getDate("birthday");
+                String gender = rs.getString("gender");
+                Integer classId = rs.getInt("class_id");
+                Student s = new Student(id,name,email,birthday,gender,classId);
+                list.add(s);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        tbstu.setItems(list);
     }
 }
